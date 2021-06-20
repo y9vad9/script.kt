39ce4b53a79f77815563b450323b0250d223f34e
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-20 18:49:34 +0300

    feat: added new resolvers

--- src/main/kotlin/fun/kotlingang/kscript/configuration/impls/KScriptConfigurationImpl.kt
+++ src/main/kotlin/fun/kotlingang/kscript/configuration/impls/KScriptConfigurationImpl.kt
@@ -2,13 +2,13 @@ package `fun`.kotlingang.kscript.configuration.impls
 
 import `fun`.kotlingang.kscript.KScriptSource
 import `fun`.kotlingang.kscript.configuration.BaseClass
-import `fun`.kotlingang.kscript.configuration.ExternalResolver
 import `fun`.kotlingang.kscript.configuration.ImplicitReceiver
 import `fun`.kotlingang.kscript.configuration.KScriptConfiguration
+import `fun`.kotlingang.kscript.configuration.KScriptResolvers
 import java.io.File
 import kotlin.reflect.KClass
 
-internal class KScriptConfigurationImpl : KScriptConfiguration {
+internal class KScriptConfigurationImpl(override var externalResolvers: KScriptResolvers = KScriptResolvers()) : KScriptConfiguration {
     @OptIn(`fun`.kotlingang.kscript.annotations.UnsafeConstructorArgs::class)
     override var baseClass: BaseClass? = null
     override val implicitReceivers: MutableSet<ImplicitReceiver<*>> = mutableSetOf()
@@ -17,7 +17,6 @@ internal class KScriptConfigurationImpl : KScriptConfiguration {
         implicitReceivers.add(ImplicitReceiver(kClass, instance))
     }
 
-    override var externalResolver: ExternalResolver = MavenExternalResolver()
     override val classpath: MutableSet<File> = mutableSetOf()
     override val defaultImports: MutableSet<String> = mutableSetOf()
 
--- src/main/kotlin/fun/kotlingang/kscript/dependencies/resolver/KScriptCachedMavenResolver.kt
+++ src/main/kotlin/fun/kotlingang/kscript/dependencies/resolver/KScriptCachedMavenResolver.kt
@@ -1,19 +1,17 @@
-package `fun`.kotlingang.kscript.configuration.impls
+package `fun`.kotlingang.kscript.dependencies.resolver
 
-import `fun`.kotlingang.kscript.configuration.ExternalResolver
-import `fun`.kotlingang.kscript.configuration.KScriptExternals
 import `fun`.kotlingang.kscript.dependencies.Dependency
+import `fun`.kotlingang.kscript.dependencies.resolver.metadata.MavenMetadata
 import `fun`.kotlingang.kscript.dependencies.toDependency
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.io.File
 
 private const val DELIMITER_BETWEEN_DEPENDENCY_AND_FILES = ";"
 private const val FILES_DELIMITER = "|"
 
-public class CachedMavenExternalResolver(private val folder: File) : ExternalResolver {
+public class KScriptCachedMavenResolver(private val cacheFolder: File) : KScriptResolver<MavenMetadata> {
+
     private fun getCachedDependencies(): List<Pair<Dependency, Collection<File>>> =
-        File(folder, "cached_dependencies.txt")
+        File(cacheFolder, "cached_dependencies.txt")
             .readLines()
             .map {
                 val parts = it.split(DELIMITER_BETWEEN_DEPENDENCY_AND_FILES)
@@ -23,9 +21,9 @@ public class CachedMavenExternalResolver(private val folder: File) : ExternalRes
                 )
             }
 
-    override suspend fun resolve(externals: KScriptExternals): Collection<File> = withContext(Dispatchers.IO) {
+    override suspend fun resolve(metadata: MavenMetadata): Collection<File> {
         val cached = getCachedDependencies()
-        return@withContext externals.dependencies.map { dependency ->
+        return metadata.dependencies.map { dependency ->
             cached.find { it.first.toString() == dependency.toString() }
         }.mapNotNull { it?.second }
             .flatten()
