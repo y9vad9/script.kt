e52677e322aebedc9467158043b6839e83ce06a4
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-20 18:47:46 +0300

    refactor: `toMD5` moved to top-level due to unnecessary it's consist in object

--- src/main/kotlin/fun/kotlingang/kscript/internal/Digest.kt
+++ src/main/kotlin/fun/kotlingang/kscript/internal/Digest.kt
@@ -3,9 +3,7 @@ package `fun`.kotlingang.kscript.internal
 import java.math.BigInteger
 import java.security.MessageDigest
 
-internal object Digest {
-    fun String.toMD5(): String {
-        val md = MessageDigest.getInstance("MD5")
-        return BigInteger(1, md.digest(this.toByteArray())).toString(16).padStart(32, '0')
-    }
+internal fun String.toMD5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(this.toByteArray())).toString(16).padStart(32, '0')
 }
