4e8d4bcf30805dc948424393c175f113f6a7948d
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-20 20:36:58 +0300

    fix: annotation target fix

--- src/main/kotlin/fun/kotlingang/kscript/annotations/ImportFile.kt
+++ src/main/kotlin/fun/kotlingang/kscript/annotations/ImportFile.kt
@@ -5,5 +5,7 @@ package `fun`.kotlingang.kscript.annotations
  * @param path - path to file.
  */
 @ScriptAnnotation
+@Target(AnnotationTarget.FILE)
 @Retention(AnnotationRetention.SOURCE)
+@Repeatable
 public annotation class ImportFile(val path: String)
